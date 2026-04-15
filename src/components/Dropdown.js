f9a2c2f857ec122bb4ff53ea5035eb3656560d5e
Alton Michaux <alton.michaux@yahoo.com>
2026-04-14 21:53:06 -0400

    updated app stack

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,11 +5,11 @@ import styles from "../css/Dropdown.module.css"
 const Dropdown = () => {
   const rubyRef = useRef(null)
   const nodeRef = useRef(null)
-  const nextRef = useRef(null)
+  const djangoRef = useRef(null)
 
   const handleSelect = (selected) => {
     const dropdownValue = selected.target.value
-    const refs = { "Node/Express": nodeRef, "Ruby on Rails": rubyRef, "Next.js": nextRef }
+    const refs = { "Node/Express": nodeRef, "Ruby on Rails": rubyRef, "Django": djangoRef }
 
     Object.entries(refs).forEach(([key, ref]) => {
       if (ref.current) {
@@ -25,14 +25,14 @@ const Dropdown = () => {
         <select className={[styles.projectPulldown]} id="projects" defaultValue={"undefined"} onChange={selected => handleSelect(selected)}>
           <option value={"undefined"} className={styles.projects} disabled>Select a stack</option>
           <option value="Node/Express" className={styles.projects}>Node/Express</option>
-          <option value="Next.js" className={styles.projects}>Next.js</option>
+          <option value="Django" className={styles.projects}>Django</option>
           <option value="Ruby on Rails" className={styles.projects}>Ruby on Rails</option>
         </select>
       </div>
       <AppGrid
         rubyRef={rubyRef}
         nodeRef={nodeRef}
-        nextRef={nextRef}
+        djangoRef={djangoRef}
       ></AppGrid>
     </>
   )
